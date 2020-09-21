8b98ab1188dc8fa5ce054bc4cda74ed2773f5ad6
GeisonRDS <geisonrobertodasilva@gmail.com>
2020-09-21 01:05:34 -0300

    first commit

---
+++ bd/Livros.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize');
+const conexao = require('./conexao');
+
+const Livros = conexao.define('livros', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    titulo: Sequelize.STRING,
+    autor: Sequelize.STRING,
+    editora: Sequelize.STRING,
+    ano: Sequelize.INTEGER
+});
+
+Livros.sync({force: false});
+
+module.exports = Livros;
